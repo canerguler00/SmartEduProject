cb01ad2356e7e652b54b0d1cc857054e4c8a60cb
caner guler <c.guler58@gmail.com>
2022-07-26 23:10:05 +0700

    second commit

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const app = express();
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
+app.use(express.static('public'));
+
+//Routes
 app.get('/', (req, res) => {
-  res.send('index sayfası');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
+});
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "index"
+  });
 });
 
 const port = 3000;
