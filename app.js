6b6705279df89a5e7dcd75860a693c6520b84489
caner guler <c.guler58@gmail.com>
2022-07-31 15:53:57 +0700

    make it better

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const methodOverride = require("method-override")
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
+const categoryRoute = require("./routes/categoryRoute")
+const userRoute = require("./routes/userRoute")
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb+srv://dbUser:uObtVIOQ2CVeUIh2@cluster0.0stl7ez.mongodb.net/?retryWrites=true&w=majority',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log("DB Connected Successfuly");
+})
 
 //Template Engine
 app.set('view engine', 'ejs');
 
+//Global variable
+
+global.userIN = null;
+
+
 //Middlewares
+
 app.use(express.static('public'));
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: 'my_keyboard_cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' })  
+}))
+app.use(flash());
+app.use((req, res, next)=>{
+  res.locals.flashMessages = req.flash();
+  next()
+})
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"]  
+}))
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: "index"
-  });
-});
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-    page_name: "index"
-  });
-});
+app.use("*", (req, res, next)=> {
+  userIN = req.session.userID;
+  next();
+})
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
-const port = 3000;
+const port = process.env.PORT  || 5000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
 });
---
+++ routes/pageRoute.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const pageController = require("../controllers/pageController")
+const redirectWiddleware = require("../middlewares/redirectMiddleware")
+
+const router = express.Router();
+
+router.route("/").get(pageController.getIndexPage);
+router.route("/about").get(pageController.getAboutPage);
+router.route("/register").get(redirectWiddleware, pageController.getRegisterPage);
+router.route("/login").get(redirectWiddleware, pageController.getLoginPage);
+router.route("/contact").get(pageController.getContactPage);
+router.route("/contact").post(pageController.sendEmail);
+
+module.exports = router;
